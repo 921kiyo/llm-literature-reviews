ef99c386c27bbba3fb85b6d7a3b1190479f544f7
Kiyo Kunii <kiyo921@hotmail.co.jp>
2023-05-07 20:54:53 +0100

    update

--- backend/src/server/app.py
+++ backend/src/server/app.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import arxiv
 import tempfile
 
+from question_answer_pipeline.src.utils import qa_abstracts
+
 app = FastAPI()
 
 origins = [
@@ -23,6 +25,17 @@ app.add_middleware(
 class SearchItem(BaseModel):
     search_term: str
 
+def parse_search_results(results):
+    output = []
+    for result in results:
+        output.append({
+            'published': str(result.published),
+            "entry_id": result.entry_id,
+            'summary': result.summary,
+            'title': result.title,
+            "authors": [{'name': author.name} for author in result.authors]
+        })
+    return output
 
 @app.get("/")
 async def root():
@@ -32,10 +45,12 @@ async def root():
 async def search_paper(message: SearchItem):
     search_results = arxiv.Search(
         query = message.search_term,
-        max_results = 5,
+        max_results = 1,
         sort_by = arxiv.SortCriterion.Relevance,
         sort_order = arxiv.SortOrder.Descending
     ).results()
+    search_results_list = parse_search_results(search_results)
+    qa_abstracts(message.search_term, search_results_list)
 
     # TODO: try webscraping instead as it might be faster
     with tempfile.TemporaryDirectory() as tempdir:
